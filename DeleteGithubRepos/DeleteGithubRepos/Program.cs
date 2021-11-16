93d4dd465a09b4ddcbb89c5aef5cad08d2deb878
Robin La Rondelle <robinlarondelle@hotmail.com>
2021-11-16 01:28:52 +0100

    Added Readme and better console hints

--- DeleteGithubRepos/DeleteGithubRepos/Program.cs
+++ DeleteGithubRepos/DeleteGithubRepos/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System.Text;
 using System.Text.Json;
 
+Console.WriteLine("This script will fetch all your owned repositories from Github (public AND private).");
+Console.WriteLine("Press 'y' to confirm deletion. Press any other key to skip to next repo.");
+Console.WriteLine("Re-run the tool if you have more than 30 repos, because of pagination.\n\n");
+
 using (var client = new HttpClient())
 {
     string username = "robinlarondelle";
-    string token = ""; // <-- your token here
+    string token = ""; // <-- your personal access token here
     byte[] byteArray = Encoding.ASCII.GetBytes($"{username}:{token}");
 
     client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-    client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+    client.DefaultRequestHeaders.Add("User-Agent", "Github Repository Deletion Tool");
 
     var stringTask = client.GetStreamAsync("https://api.github.com/user/repos?affiliation=owner");
 
@@ -21,17 +25,23 @@ using (var client = new HttpClient())
         {
             foreach (var repo in repos)
             {
-                Console.WriteLine($"Delete {repo.name}?");
-                var input = Console.ReadLine();
+                Console.WriteLine($"Delete {repo.name}? [y/N]");
+                var input = Console.ReadLine()?.ToLower();
 
                 if (input == "y")
                 {
-                    Console.WriteLine($"Deleting {repo.name}");
-                    var url = $"https://api.github.com/repos/{username}/{repo.name}";
-                    var deleteTask = client.DeleteAsync(url);
-                    var msg = await deleteTask;
-                    var deleteResponse = await msg.Content.ReadAsStringAsync();
-                    Console.WriteLine(deleteResponse ?? $"Deleted {repo.name}\n");
+                    Console.WriteLine($"Are you sure you want to delete {repo.name}? This can not be undone! [y/N]");
+                    input = Console.ReadLine()?.ToLower();
+
+                    if (input == "y")
+                    {
+                        Console.WriteLine($"Deleting {repo.name}");
+                        var url = $"https://api.github.com/repos/{username}/{repo.name}";
+                        var deleteTask = client.DeleteAsync(url);
+                        var msg = await deleteTask;
+                        var deleteResponse = await msg.Content.ReadAsStringAsync();
+                        Console.WriteLine(deleteResponse ?? $"Deleted {repo.name}\n");
+                    }
                 }
             }
         }
---
+++ DeleteGithubRepos/readme.md
@@ -0,0 +1,6 @@
+# Delete Github Repos tool
+This is a small quick and dirty tool to delete owned Github repositories.
+
+Make sure to create a Personal Access Token (https://github.com/settings/tokens) with Delete Repository rights and add that to the script.
+
+Rerun the script if you have more than 30 repos.
